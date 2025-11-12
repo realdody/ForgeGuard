87ee5d8f83ed7e1c9469b950e2fb14c6cdbd1c7c
Riever <riever.holic@gmail.com>
2025-11-11 20:17:12 -0600

    Add spoofed IP

--- src/main/java/dev/riever/forgeguard/mixin/NetworkManagerMixin.java
+++ src/main/java/dev/riever/forgeguard/mixin/NetworkManagerMixin.java
@@ -1,7 +1,13 @@
 package dev.riever.forgeguard.mixin;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import dev.riever.forgeguard.ForgeGuardNetworkManager;
 import dev.riever.forgeguard.ForwardedProfile;
@@ -25,5 +31,13 @@ public class NetworkManagerMixin implements ForgeGuardNetworkManager {
     @Override
     public void forgeguard$setForwardedProfile(ForwardedProfile forwardedProfile) {
         forgeguard$forwardedProfile = forwardedProfile;
+    }
+
+    @Inject(method = "getSocketAddress", at = @At("HEAD"), cancellable = true)
+    public void forgeguard$onGetSocketAddress(CallbackInfoReturnable<SocketAddress> cir) {
+        String address = forgeguard$forwardedProfile != null ? forgeguard$forwardedProfile.address() : null;
+        if (address != null) {
+            cir.setReturnValue(new InetSocketAddress(address, 0));
+        }
     }
 }
